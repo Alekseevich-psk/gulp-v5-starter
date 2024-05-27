9508ee0ebb5b6a2d245913fddeecf813db1467ef
Andrew <ap-pskov@ya.ru>
2024-05-27 15:20:38 +0300

    Update zip.mjs

--- gulp/zip.mjs
+++ gulp/zip.mjs
@@ -5,7 +5,9 @@ import { paths } from "./config.mjs";
 import zip from "gulp-zip";
 
 const zipDist = () => {
-    return src(paths.dist + "**/*.{html,jpg,png,svg,gif,ico,webp,video,js,css}")
+    return src(paths.dist + "**/*.{html,jpg,png,svg,gif,ico,webp,video,js,css}", {
+        encoding: false,
+    })
         .pipe(zip("archive.zip"))
         .pipe(dest(paths.dist));
 };
