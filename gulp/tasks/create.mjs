49e54fe33ff45e7928c75b84767068fe8c023f9c
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2025-04-27 21:57:24 +0300

    Update create.mjs

--- gulp/tasks/create.mjs
+++ gulp/tasks/create.mjs
@@ -9,7 +9,7 @@ import { paths } from "../config/config.mjs";
 const createFolder = (path, nameFolder) => {
     const dir = path + "/" + nameFolder.slice(2);
 
-    fs.mkdir(dir, (err) => {
+    fs.mkdirSync(dir, (err) => {
         if (err) return console.error(err);
         console.log("Directory created successfully!");
     });
